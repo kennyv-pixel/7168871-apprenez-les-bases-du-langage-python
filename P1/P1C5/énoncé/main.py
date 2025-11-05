d8484202cacc90de4957db3e14249779a75df46f
kennyv-pixel <kennyv@live.fr>
2025-11-06 00:28:31 +0100

    Add personal details and print statement
    Added personal information and print statement to main.py.

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,10 @@
-# Écrivez votre code ici !
+nom ="Kenny Vincent"
+age= 33
+taille= 1.75
+est_etudiant= True
+print(f"je m'appelle {nom} j'ai {age}, je fais{1.75}cm et je suis {est_etudiant }")
+
+type(nom)
+type(age)
+type(taille)
+type(est_etudiant)
