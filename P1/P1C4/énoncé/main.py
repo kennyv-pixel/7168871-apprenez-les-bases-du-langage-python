1138dc8f764ee6b52e96a9185a6e33ee25ea291c
kennyv-pixel <kennyv@live.fr>
2025-11-05 23:48:43 +0100

    Add personal information and print statements
    Corrected variable name from 'statue_sociale' to 'statut_sociale' and updated age.

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,6 @@
-# Écrivez votre code ici !
+nom="Vincent Kenny"
+age="33"
+statut_sociale="célibataire"
+print(f" je m'appelle {nom} et j'ai {age} ans et je suis {statue_sociale}")
+age="43"
+print(f" je m'appelle {nom} et j'ai {age} ans et je suis {statue_sociale}")
