1ee48bc0dce4ffe50f434a7b79932fa9d97d4d98
kennyv-pixel <kennyv@live.fr>
2025-11-06 23:47:56 +0100

    Implement fruit list operations and display results
    Manipulate a list of fruits by adding, removing, and modifying elements, then print the count and sorted list.

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,7 @@
-# Écrivez votre code ici !
+fruits=["pomme", "banane", "orange"]
+fruits.append("kiwi")
+fruits.remove("orange")
+fruits[1] = "ananas"
+print("La liste de fruit contient", len(fruits), "éléments")
+fruits.sort()
+print(fruits)
