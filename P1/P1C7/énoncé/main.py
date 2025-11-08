28a38dd1fe5cf2b93b89791e40a7716aae0a7860
kennyv-pixel <kennyv@live.fr>
2025-11-08 17:47:59 +0100

    Update main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,10 @@
-# Écrivez votre code ici !
+fruits= {
+  "pomme": "rouge",
+  "banane" : "jaune",
+  "orange" : "orange"
+}
+fruits["kiwi"]= "vert"
+fruits["banane"] = couleur_banane
+fruits["pomme"]= "vert"
+del fruits["banane"]
+print(fruits.keys())
